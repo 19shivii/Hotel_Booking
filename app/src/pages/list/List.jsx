8214e6ac0bed43b43856be17c679e1e03af6672e
Shivani Gupta <gshivani368@gmail.com>
2023-02-11 21:44:02 +0530

    header commit

---
+++ app/src/pages/list/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import Navbar from '../../components/navbar/Navbar'
+
+const List = () => {
+  return (
+    <><Navbar /><Header type="list" /></>
+  )
+}
+
+export default List
