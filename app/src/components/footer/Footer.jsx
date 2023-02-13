398beadb73d40e9d72032a4a62a6029b90c93ca4
Shivani Gupta <gshivani368@gmail.com>
2023-02-13 19:58:25 +0530

    home2 commit

---
+++ app/src/components/footer/Footer.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import "./Footer.css"
+const Footer = () => {
+  return (
+    <div className="footer">
+        <div className="fLists">
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+            <ul className="fList">
+                <li className="fListItem">Countries</li>
+                <li className="fListItem">Regions</li>
+                <li className="fListItem">Cities</li>
+                <li className="fListItem">District</li>
+                <li className="fListItem">Airports</li>
+                <li className="fListItem">Hotels</li>
+            </ul>
+        </div>
+        <div className="fText">Copyright © 2023 Book Your Dream</div>
+    </div>
+  )
+}
+
+export default Footer
--- app/src/pages/home/Home.jsx
+++ app/src/pages/home/Home.jsx
@@ -3,8 +3,10 @@ import Header from '../../components/header/Header';
 import Navbar from '../../components/navbar/Navbar';
 import Features from '../../components/features/Features'
 import FeaturedProp from '../../components/featuredprop/FeaturedProp'
+import MailList from '../../components/mailList/MailList'
 import './Home.css';
 import PropertyList from '../../components/propertylist/PropertyList';
+import Footer from '../../components/footer/Footer';
 const Home = () => {
   return (
     <div>
@@ -16,6 +18,8 @@ const Home = () => {
         <PropertyList/>
         <h1 className="homeTitle">Homes guest love</h1>
          <FeaturedProp/>
+         <MailList/>
+<Footer/>
       </div>
     </div>
 
